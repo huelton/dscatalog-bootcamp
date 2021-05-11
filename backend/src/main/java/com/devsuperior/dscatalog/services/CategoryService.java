abb6d0d66f6fd57f2547ebefa99d6659d667b37f
Huelton Santos <hueltondihon@gmail.com>
2021-05-11 16:09:36 -0300

    Update Category

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,7 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
-import com.devsuperior.dscatalog.services.exceptions.EntityNotFoundException;
+import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class CategoryService {
@@ -28,17 +30,30 @@ public class CategoryService {
 	@Transactional(readOnly = true)
 	public CategoryDTO findById(Long id) {
 		Optional<Category> obj = categoryRepository.findById(id);
-		Category cat = obj.orElseThrow(() -> new EntityNotFoundException("Entity Not Found"));
+		Category cat = obj.orElseThrow(() -> new ResourceNotFoundException("Entity Not Found"));
 		return new CategoryDTO(cat);
 
 	}
 
-	@Transactional()
+	@Transactional
 	public CategoryDTO save(CategoryDTO dto) {
 		Category entity = new Category();
 		entity.setName(dto.getName());
 		categoryRepository.save(entity);		
 		return new CategoryDTO(entity);
+	}
+
+	@Transactional
+	public CategoryDTO update(Long id, CategoryDTO dto) {		
+		try {
+			Category entity = categoryRepository.getOne(id);
+			entity.setName(dto.getName());
+			entity = categoryRepository.save(entity);
+			return new CategoryDTO(entity);
+			
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id Not Found: "+ id);
+		}
 	}
 
 }
