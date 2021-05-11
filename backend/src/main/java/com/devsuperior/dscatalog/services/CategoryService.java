618385f27efd7f0a425b039ee6f2ec824bfe2c2d
Huelton Santos <hueltondihon@gmail.com>
2021-05-11 16:35:05 -0300

    Auditing data

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -7,12 +7,15 @@ import java.util.stream.Collectors;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
+import com.devsuperior.dscatalog.services.exceptions.DatabaseException;
 import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -39,20 +42,31 @@ public class CategoryService {
 	public CategoryDTO save(CategoryDTO dto) {
 		Category entity = new Category();
 		entity.setName(dto.getName());
-		categoryRepository.save(entity);		
+		categoryRepository.save(entity);
 		return new CategoryDTO(entity);
 	}
 
 	@Transactional
-	public CategoryDTO update(Long id, CategoryDTO dto) {		
+	public CategoryDTO update(Long id, CategoryDTO dto) {
 		try {
 			Category entity = categoryRepository.getOne(id);
 			entity.setName(dto.getName());
 			entity = categoryRepository.save(entity);
 			return new CategoryDTO(entity);
-			
+
 		} catch (EntityNotFoundException e) {
-			throw new ResourceNotFoundException("Id Not Found: "+ id);
+			throw new ResourceNotFoundException("Id Not Found: " + id);
+		}
+	}
+
+	public void delete(Long id) {
+		try {
+			categoryRepository.deleteById(id);
+
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id Not Found: " + id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity Violation ");
 		}
 	}
 
